fa7be58498b1604446121be5a79e17a6b207eebb
vonElfvin <elvin.granat@gmail.com>
2018-01-05 11:14:27 +0100

    Done with Assignment 2 Lab 3

---
+++ Lab 2/assignment4.R
@@ -0,0 +1,51 @@
+#### Assignment 4
+### Libraries
+library(fastICA)
+library(pls)
+
+### Setup
+data.spectra = read.csv2("NIRSpectra.csv")
+data = data.spectra
+data$Viscosity = c()
+
+### Functions
+
+### Implementation
+## Task 1 - PCA
+PCA = prcomp(data)
+# Extract eigenvalues
+λ = PCA$sdev^2
+variation.proportion = λ/sum(λ)
+# screeplot(PCA)
+barplot(variation.proportion[1:10], ylim=c(0,1), col="forestgreen", 
+        main="Variation explanaition proportions for different eigenvalues", 
+        xlab="PCi", ylab="Variation proportion") # The plot shows that 2 PC1 and PC2 should be extracted
+sum(variation.proportion[1:2]) # 99.5957% of the total variance is explained by the first 2 PCs
+
+# Scores - There seems to be 2-7 unusual diesiel fuels according to this plot
+plot(PCA$x[,1], PCA$x[,2], xlab="Z1", ylab="Z2", main="Projected Values, PCA") # 2 "strong" outliers, 5-7 "medium" outliers
+
+## Task 2 - Trace Plots
+U = PCA$rotation # rotation matrix
+
+# Tracing plots
+plot(U[,1], main="Traceplot for PC1", ylim=c(-0.11,0.11), ylab="Projection/Rotation Value")
+plot(U[,2], main="Traceplot for PC2", ylim=c(-0.3, 0.3), ylab="Projection/Rotation Value") # the last ~10 feutures mainly explain
+
+## Task 3 - Indipendent Component Analysis (ICA)
+set.seed(12345)
+# 2 components selected -> n.comp=2
+ICA = fastICA(data, n.comp=2, alg.typ="parallel", fun="logcosh", alpha=1, method="R", row.norm=FALSE, maxit=200, tol=0.0001, verbose=TRUE)
+# W' = K * W
+Wtick = ICA$K%*%ICA$W
+plot(Wtick[,1], main="Traceplot, W' column 1", ylim=c(-1, 1), ylab="Projection/Rotation Value")
+plot(Wtick[,2], main="Traceplot, W' column 2", ylim=c(-11,11), ylab="Projection/Rotation Value") 
+# Comment: the "opposite" to PCA, similar information
+
+# Scores
+plot(ICA$S[,1], ICA$S[,2], xlab="Z1", ylab="Z2", main="Projected Values, ICA") # ICA, 
+
+# Task 4 - PCR
+set.seed(12345)
+PCR = pcr(Viscosity~., data=data.spectra, validation="CV")
+validationplot(PCR, val.type="MSEP", main="Dependence of MSEP (Mean Squared Error of Prediction) on #components")
---
+++ Lab 3/assignment2.R
@@ -0,0 +1,67 @@
+#### Assignment 2 - Neural Networks
+### Libraries
+library(neuralnet)
+
+### Setup
+set.seed(1234567890)
+rad = runif(50, 0, 10) # 50 radians between 0 and 10
+data = data.frame(rad, sin=sin(rad))
+#n = dim(data)[1]
+#ids = sample(1:n, n/2)
+#train = data[ids,]
+#valid = data[-ids,]
+train = data[1:25,]
+valid = data[26:50,]
+y.train = train$sin
+y.valid = valid$sin
+
+### Functions
+mse = function(y, yfit){
+  return(mean((y-yfit)^2))
+}
+
+### Implementation
+w.init = runif(31, -1, 1)
+m = 10
+threshold = numeric()
+mse.train = numeric()
+mse.valid = numeric()
+
+for(i in 1:m){
+  nn = neuralnet(formula=sin~rad, data=train, hidden=c(10), threshold = i/1000, startweights = w.init)
+  yfit.train = compute(nn, covariate=train$rad)$net.result
+  yfit.valid = compute(nn, covariate=valid$rad)$net.result
+  threshold[i] = i/1000
+  mse.train[i] = mse(y.train, yfit.train)
+  mse.valid[i] = mse(y.valid, yfit.valid) 
+}
+# Plot the MSEs of the different data sets
+plot(threshold, mse.train, xlim=c(threshold[m], threshold[1]), type="b", col="blue", ylim=c(0, 0.0012),
+     main="MSEs vs. Threshold", ylab="MSE")
+points(threshold, mse.valid, xlim=c(threshold[m], threshold[1]), type="b", col="green")
+# Comment: Best threshhold = 4/1000, i = 4
+
+# Neural Network with best threshhold
+best.threshold = which.min(mse.valid)
+nn = neuralnet(sin~rad, data=data, hidden=c(10), threshold=threshold[best.threshold], startweights=w.init)
+plot(nn)
+
+# Predictions
+pred.all = prediction(nn)$rep1
+yfit.train = compute(nn, train$rad)$net.result
+yfit.valid = compute(nn, valid$rad)$net.result
+
+# For All
+plot(data, col="red", ylim=c(-1, 2), main="Predictions All")
+points(pred.all, col="blue")
+legend("topright", legend=c("Predictions (All)", "Actual (All)"), col=c("blue","red"), lty=1)
+
+# For Training
+plot(data, col="red", ylim=c(-1, 2), main="Predictions Training")
+points(train$rad, yfit.train, col="blue")
+legend("topright", legend=c("Predictions (Training)", "Actual (All)"), col=c("blue","red"), lty=1)
+
+# For validation
+plot(data, col="red", ylim=c(-1, 2), main="Predictions Validation")
+points(valid$rad, yfit.valid, col="green")
+legend("topright", legend=c("Predictions (Validation)", "Actual (All)"), col=c("green","red"), lty=1)
